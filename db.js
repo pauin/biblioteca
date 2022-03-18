9ef8eef7de7fbba821f40c4b1cbee3aea0b62d2c
Paula <p.e.inzunza@hotmail.com>
2022-03-18 18:44:13 -0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,47 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "biblioteca",
+    password: "1234",
+    port: "5432",
+    max: 12,
+    min: 2,
+    idleTimeoutMillis: 3000,
+    connectTimeoutMillis: 2000,
+});
+
+async function add_books(title, descripcion) {
+    const client = await pool.connect();
+    const { rows } = await client.query({
+        text: `insert into libros (title, descripcion) values ($1, $2)`,
+        values: [title, descripcion],
+    });
+    client.release();
+    return rows[0];
+}
+async function mostrar() {
+    const client = await pool.connect();
+    const { rows } = await client.query(`select * from libros`);
+    client.release();
+    return rows;
+}
+async function add_autores(firstname, lastname, notes) {
+    const client = await pool.connect();
+    const { rows } = await client.query({
+        text: `insert into autores (firstname, lastname, notes) values ($1, $2, $3)`,
+        values: [firstname, lastname, notes],
+    });
+    client.release();
+    return rows[0];
+}
+async function mostrar_autores() {
+    const client = await pool.connect();
+    const { rows } = await client.query(`select * from autores`);
+    client.release();
+    return rows;
+}
+
+
+module.exports = { add_books, mostrar, add_autores, mostrar_autores }
